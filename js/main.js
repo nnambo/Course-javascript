2a471489dfc7e9dac82f30fe5fd921272f9dd7e7
nnambo <73754395+nnambo@users.noreply.github.com>
2023-05-05 04:45:04 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,55 +1,138 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  if (window.location.href.indexOf("index") > -1) {
+    $(".galeria").bxSlider({
+      mode: "fade",
+      captions: false,
+      slideWidth: 1200,
+      responsive: true,
+      pager: true,
+    });
+  }
 
-  $(".galeria").bxSlider({
-    mode: "fade",
-    captions: true,
-    slideWidth: 1200,
-    responsive: true,
-  });
+  if (window.location.href.indexOf("index") > -1) {
+    var posts = [
+      {
+        title: "Prueba de titulo 1",
+        date:
+          "Publicado el dia " +
+          moment().date() +
+          " de " +
+          moment().format("MMMM") +
+          " del " +
+          moment().format("YYYY"),
+        content:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam quisquam voluptatum dicta, perferendis sed magni reprehenderit repudiandae deleniti. Eius ratione dolores vel modi odit qui placeat libero aliquid sit illo?",
+      },
+      {
+        title: "Prueba de titulo 2",
+        date:
+          "Publicado el dia " +
+          moment().date() +
+          " de " +
+          moment().format("MMMM") +
+          " del " +
+          moment().format("YYYY"),
+        content:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam quisquam voluptatum dicta, perferendis sed magni reprehenderit repudiandae deleniti. Eius ratione dolores vel modi odit qui placeat libero aliquid sit illo?",
+      },
+      {
+        title: "Prueba de titulo 3",
+        date:
+          "Publicado el dia " +
+          moment().date() +
+          " de " +
+          moment().format("MMMM") +
+          " del " +
+          moment().format("YYYY"),
+        content:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam quisquam voluptatum dicta, perferendis sed magni reprehenderit repudiandae deleniti. Eius ratione dolores vel modi odit qui placeat libero aliquid sit illo?",
+      },
+      {
+        title: "Prueba de titulo 4",
+        date:
+          "Publicado el dia " +
+          moment().date() +
+          " de " +
+          moment().format("MMMM") +
+          " del " +
+          moment().format("YYYY"),
+        content:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam quisquam voluptatum dicta, perferendis sed magni reprehenderit repudiandae deleniti. Eius ratione dolores vel modi odit qui placeat libero aliquid sit illo?",
+      },
+      {
+        title: "Prueba de titulo 5",
+        date:
+          "Publicado el dia " +
+          moment().date() +
+          " de " +
+          moment().format("MMMM") +
+          " del " +
+          moment().format("YYYY"),
+        content:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam quisquam voluptatum dicta, perferendis sed magni reprehenderit repudiandae deleniti. Eius ratione dolores vel modi odit qui placeat libero aliquid sit illo?",
+      },
+    ];
 
-
-  
-  var posts =[
-    {title: 'Prueba de titulo 1',
-date: 'Publicado el dia '+ moment().date()+ " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
-content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam quisquam voluptatum dicta, perferendis sed magni reprehenderit repudiandae deleniti. Eius ratione dolores vel modi odit qui placeat libero aliquid sit illo?'},
-    {title: 'Prueba de titulo 2',
-    date: 'Publicado el dia '+ moment().date()+ " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
-    content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam quisquam voluptatum dicta, perferendis sed magni reprehenderit repudiandae deleniti. Eius ratione dolores vel modi odit qui placeat libero aliquid sit illo?'},
-    {title: 'Prueba de titulo 3',
-    date: 'Publicado el dia '+ moment().date()+ " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
-    content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam quisquam voluptatum dicta, perferendis sed magni reprehenderit repudiandae deleniti. Eius ratione dolores vel modi odit qui placeat libero aliquid sit illo?'},
-    {title: 'Prueba de titulo 4',
-    date: 'Publicado el dia '+ moment().date()+ " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
-    content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam quisquam voluptatum dicta, perferendis sed magni reprehenderit repudiandae deleniti. Eius ratione dolores vel modi odit qui placeat libero aliquid sit illo?'},
-  {title: 'Prueba de titulo 5',
-  date: 'Publicado el dia '+ moment().date()+ " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
-  content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam quisquam voluptatum dicta, perferendis sed magni reprehenderit repudiandae deleniti. Eius ratione dolores vel modi odit qui placeat libero aliquid sit illo?'},
-];
-
-posts.forEach((item, index) => {
-  var post = `
+    posts.forEach((item, index) => {
+      var post = `
   <article class="post">
   <h2>${item.title}</h2>
   <span class="date">${item.date}</span>
   <p>${item.content}</p>
-  <a href="#" class="button">Leer mas</a>
+  <a href="#" class="button-more">Leer mas</a>
   </article>`;
-  $("#posts").append(post);
-});
+      $("#posts").append(post);
+    });
+  }
+  //selector de tema
+  var theme = $("#theme");
 
-//selector de tema
-var theme = $("#theme");
+  $("#to-green").click(function () {
+    theme.attr("href", "css/green.css");
+  });
+  $("#to-red").click(function () {
+    theme.attr("href", "css/red.css");
+  });
+  $("#to-blue").click(function () {
+    theme.attr("href", "css/blue.css");
+  });
 
-$("#to-green").click(function(){
-  theme.attr("href", "css/green.css");
-});
-$("#to-red").click(function(){
-  theme.attr("href", "css/red.css");
-});
-$("#to-blue").click(function(){
-  theme.attr("href", "css/blue.css");
-});
-});
+  //login falso
+  $("#login form").submit(function () {
+    var form_name = $("#form_name").val();
+    localStorage.setItem("form_name", form_name);
+  });
+  var form_name = localStorage.getItem("form_name");
+  if (form_name != null && form_name != "undefined") {
+    var about_parrafo = $("#about p");
+    about_parrafo.html("<br><strong>Bienvenido, " + form_name + "</strong>");
+    about_parrafo.append("<a href='#' id='logout'>Cerrar sesion</a>");
+    $("#login").hide();
 
+    $("#logout").click(function () {
+      localStorage.clear();
+      location.reload();
+    });
+  }
 
+  //acordeon
+  if (window.location.href.indexOf("about") > -1) {
+    $("#acordeon").accordion();
+  }
+
+  //reloj
+  if (window.location.href.indexOf("reloj") > -1) {
+    setInterval(function () {
+      var reloj = moment().format("hh:mm:ss");
+      $("#reloj").html(reloj);
+    }, 1000);
+  }
+  //validacion
+  if (window.location.href.indexOf("contact") > -1) {
+    $.validate({
+      lang: "es",
+      errorMessagePosition: "top",
+      scrollToTopOnError: true,
+    });
+  }
+});
